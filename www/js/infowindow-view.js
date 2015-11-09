67db9596026ce57c65cf7c371fb16757036845e2
Dave Burdick <daveburdick@gmail.com>
2015-11-09 16:21:01 -0700

    Reposition search bar

--- www/js/infowindow-view.js
+++ www/js/infowindow-view.js
@@ -1,26 +1,26 @@
 var setMarkers = [
-	{
-		title: 'Belmar Library',
-		url: 'http://www.jeffcolibrary.org/locations/belmar-library',
-		address: '555 S. Allison Pkwy., Lakewood, CO 80226',
-		blurb: 'The roof is shaped like an open book.',
-		lat: 39.706475,
-		lng: -105.084184
-	},
-	{
-		title: 'Caution Brewing Co.',
-		url: 'http://www.cautionbrewingco.com/',
-		address: '1057 S. Wadsworth Blvd. #60, Lakewood, CO 80226',
-		blurb: 'Really low-key, semi-garage brewery vibe.',
-		lat: 39.698176,
-		lng: -105.082173
-	},
-	{
-		title: 'Dark Matter Games',
-		url: 'http://dmgcolorado.com/',
-		address: '1050 S. Wadsworth Blvd., Lakewood, CO 80226',
-		blurb: 'Never been here, wonder how the selection is.',
-		lat: 39.698142,
-		lng: -105.080060
-	}
+		{
+			title: 'Belmar Library',
+			url: 'http://www.jeffcolibrary.org/locations/belmar-library',
+			address: '555 S. Allison Pkwy., Lakewood, CO 80226',
+			blurb: 'The roof is shaped like an open book.',
+			lat: 39.706475,
+			lng: -105.084184
+		},
+		{
+			title: 'Caution Brewing Co.',
+			url: 'http://www.cautionbrewingco.com/',
+			address: '1057 S. Wadsworth Blvd. #60, Lakewood, CO 80226',
+			blurb: 'Really low-key, semi-garage brewery vibe.',
+			lat: 39.698176,
+			lng: -105.082173
+		},
+		{
+			title: 'Dark Matter Games',
+			url: 'http://dmgcolorado.com/',
+			address: '1050 S. Wadsworth Blvd., Lakewood, CO 80226',
+			blurb: 'Never been here, wonder how the selection is.',
+			lat: 39.698142,
+			lng: -105.080060
+		}
 	]
