aaf8cf9ccb545aadf11335d0982c1964306c3d00
Dave Burdick <daveburdick@gmail.com>
2015-11-09 14:31:23 -0700

    Get markers dynamically

---
+++ www/js/infowindow-view.js
@@ -0,0 +1,13 @@
+var markers = [
+	{
+		title: 'Belmar Library',
+		url: 'http://www.jeffcolibrary.org/locations/belmar-library',
+		address: '555 S. Allison Pkwy, Lakewood, CO 80226',
+		blurb: 'The roof is shaped like an open book.',
+		lat: 39.706475,
+		lng: -105.084184
+	},
+
+
+
+	]
