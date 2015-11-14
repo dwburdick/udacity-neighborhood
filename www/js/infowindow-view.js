85f64f5901de8f21d184e5d597675398a7c7ef24
Dave Burdick <daveburdick@gmail.com>
2015-11-14 14:27:50 -0700

    Add animation to markers when active

--- www/js/infowindow-view.js
+++ www/js/infowindow-view.js
@@ -22,7 +22,7 @@ var Model = {
 		date = yyyy + "-" + mm + "-"+ dd;
 	},
 	getMovies: function() {
-		movies = $.ajax("http://data.tmsapi.com/v1.1/movies/showings?startDate=" +
+		movies = $.ajax("http://daBROKETHISTEMPORARILYta.tmsapi.com/v1.1/movies/showings?startDate=" +
 			date + "&numDays=1&lat=39.708582&lng=-105.076251%radius=1&units=mi&api_key=5p8sgppbuvrcwt9h6szyjy3u", {
 				error: function(){
 					$showtimes.append("<br>There was a problem getting a list of films.<br> Visit the theater's site for information.");
@@ -43,6 +43,7 @@ var ViewModel = {
 		setMarkers = [
 			{
 				title: 'Belmar Library',
+				id: 'library',
 				url: 'http://www.jeffcolibrary.org/locations/belmar-library',
 				address: '555 S. Allison Pkwy., Lakewood, CO 80226',
 				blurb: 'The roof is shaped like an open book.',
@@ -51,6 +52,7 @@ var ViewModel = {
 			},
 			{
 				title: 'Belmar Park playground',
+				id: 'playground',
 				url: 'http://www.lakewood.org/BelmarPark/',
 				address: '801 S. Wadsworth Blvd., Lakewood, CO 80226',
 				blurb: 'An unusual playground with faux rocks and ropes to climb and balance on.<br> The rest of the park is cool, too.',
@@ -59,6 +61,7 @@ var ViewModel = {
 			},
 			{
 				title: 'Caution Brewing Co.',
+				id: 'caution',
 				url: 'http://www.cautionbrewingco.com/',
 				address: '1057 S. Wadsworth Blvd. #60, Lakewood, CO 80226',
 				blurb: 'Really low-key, semi-garage brewery vibe.',
@@ -67,6 +70,7 @@ var ViewModel = {
 			},
 			{
 				title: 'Century 16 Belmar',
+				id: 'century',
 				url: 'http://www.cinemark.com/theatre-detail.aspx?node_id=1683&',
 				address: '440 S. Teller St., Lakewood, CO 80226',
 				blurb: 'Located in the sprawling Belmar shopping center.',
@@ -76,6 +80,7 @@ var ViewModel = {
 			},
 			{
 				title: 'Dark Matter Games',
+				id: 'darkmatter',
 				url: 'http://dmgcolorado.com/',
 				address: '1050 S. Wadsworth Blvd., Lakewood, CO 80226',
 				blurb: 'Never been here, wonder how the selection is.',
@@ -83,7 +88,8 @@ var ViewModel = {
 				lng: -105.080060
 			}
 		]
-	}
+	},
+	clickedMarkers: [''],
 };
 
 var View = {
