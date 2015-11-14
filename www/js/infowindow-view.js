645a60fc8348cd9d894b3b79c1c60e9dc8d9ab5b
Dave Burdick <daveburdick@gmail.com>
2015-11-14 15:13:16 -0700

    Put map items in observable array

--- www/js/infowindow-view.js
+++ www/js/infowindow-view.js
@@ -37,58 +37,57 @@ var ViewModel = {
 		Model.init();
 		View.init();
 		ViewModel.markers();
-		ko.applyBindings(new ViewModel.markers);
+		ko.applyBindings(ViewModel.markers);
 	},
-	markers: function() {
-		setMarkers = [
-			{
-				title: 'Belmar Library',
-				id: 'library',
-				url: 'http://www.jeffcolibrary.org/locations/belmar-library',
-				address: '555 S. Allison Pkwy., Lakewood, CO 80226',
-				blurb: 'The roof is shaped like an open book.',
-				lat: 39.706475,
-				lng: -105.084184
-			},
-			{
-				title: 'Belmar Park playground',
-				id: 'playground',
-				url: 'http://www.lakewood.org/BelmarPark/',
-				address: '801 S. Wadsworth Blvd., Lakewood, CO 80226',
-				blurb: 'An unusual playground with faux rocks and ropes to climb and balance on.<br> The rest of the park is cool, too.',
-				lat: 39.706528,
-				lng: -105.089693
-			},
-			{
-				title: 'Caution Brewing Co.',
-				id: 'caution',
-				url: 'http://www.cautionbrewingco.com/',
-				address: '1057 S. Wadsworth Blvd. #60, Lakewood, CO 80226',
-				blurb: 'Really low-key, semi-garage brewery vibe.',
-				lat: 39.698176,
-				lng: -105.082173
-			},
-			{
-				title: 'Century 16 Belmar',
-				id: 'century',
-				url: 'http://www.cinemark.com/theatre-detail.aspx?node_id=1683&',
-				address: '440 S. Teller St., Lakewood, CO 80226',
-				blurb: 'Located in the sprawling Belmar shopping center.',
-				lat: 39.708582,
-				lng: -105.076251,
-				showtimes: true
-			},
-			{
-				title: 'Dark Matter Games',
-				id: 'darkmatter',
-				url: 'http://dmgcolorado.com/',
-				address: '1050 S. Wadsworth Blvd., Lakewood, CO 80226',
-				blurb: 'Never been here, wonder how the selection is.',
-				lat: 39.697449,
-				lng: -105.080075
-			}
-		]
-	},
+	markers: ko.observableArray([
+		{
+			title: 'Belmar Library',
+			id: 'library',
+			url: 'http://www.jeffcolibrary.org/locations/belmar-library',
+			address: '555 S. Allison Pkwy., Lakewood, CO 80226',
+			blurb: 'The roof is shaped like an open book.',
+			lat: 39.706475,
+			lng: -105.084184
+		},
+		{
+			title: 'Belmar Park playground',
+			id: 'playground',
+			url: 'http://www.lakewood.org/BelmarPark/',
+			address: '801 S. Wadsworth Blvd., Lakewood, CO 80226',
+			blurb: 'An unusual playground with faux rocks and ropes to climb and balance on.<br>' +
+			'The rest of the park is cool, too.',
+			lat: 39.706528,
+			lng: -105.089693
+		},
+		{
+			title: 'Caution Brewing Co.',
+			id: 'caution',
+			url: 'http://www.cautionbrewingco.com/',
+			address: '1057 S. Wadsworth Blvd. #60, Lakewood, CO 80226',
+			blurb: 'Really low-key, semi-garage brewery vibe.',
+			lat: 39.698176,
+			lng: -105.082173
+		},
+		{
+			title: 'Century 16 Belmar',
+			id: 'century',
+			url: 'http://www.cinemark.com/theatre-detail.aspx?node_id=1683&',
+			address: '440 S. Teller St., Lakewood, CO 80226',
+			blurb: 'Located in the sprawling Belmar shopping center.',
+			lat: 39.708582,
+			lng: -105.076251,
+			showtimes: true
+		},
+		{
+			title: 'Dark Matter Games',
+			id: 'darkmatter',
+			url: 'http://dmgcolorado.com/',
+			address: '1050 S. Wadsworth Blvd., Lakewood, CO 80226',
+			blurb: 'Never been here, wonder how the selection is.',
+			lat: 39.697449,
+			lng: -105.080075
+		}
+	]),
 	clickedMarkers: [''],
 };
 
